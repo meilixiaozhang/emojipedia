f8488eb6ffe7d5a64132ead940f5e7fd9391e135
meilixiaozhang <redpangci@gmail.com>
2020-04-24 00:16:48 +1000

    update emoji card

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -4,20 +4,43 @@ const emojipedia = [
     emoji: "💪",
     name: "Tense Biceps",
     meaning:
-      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
   },
   {
     id: 2,
     emoji: "🙏",
     name: "Person With Folded Hands",
     meaning:
-      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
   },
   {
     id: 3,
     emoji: "🤣",
     name: "Rolling On The Floor, Laughing",
     meaning:
-      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
-  }
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "😉",
+    name: "Winking Face",
+    meaning:
+      "A yellow face with a slight smile shown winking, usually its left eye. May signal a joke, flirtation, hidden meaning, or general positivity. Tone varies, including playful, affectionate, suggestive, or ironic.",
+  },
+  {
+    id: 5,
+    emoji: "🤭",
+    name: "Face with Hand Over Mouth",
+    meaning:
+      "A yellow face with a hand covering its mouth. Apple and Facebook feature simple, open eyes, suggesting someone gasping Oh my! in serious surprise, shock, concern, or apology. Other platforms depict a face with smiling eyes and/or blushing cheeks, suggesting coy laughter or embarrassment, as if cheekily saying Oops!",
+  },
+  {
+    id: 6,
+    emoji: "😅",
+    name: "Grinning Face with Sweat",
+    meaning:
+      "Has the same grin and eyes as 😄 Grinning Face With Smiling Eyes but with a single, blue bead of sweat, usually over its left eye. Intended to depict nerves or discomfort but commonly used to express a close call, as if saying Whew! and wiping sweat from the forehead.",
+  },
 ];
+
+export default emojipedia;
